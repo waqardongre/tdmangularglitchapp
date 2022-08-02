2e86b2dc7f62705f157d9b7540df33404467e852
waqardongre <waqardongre@gmail.com>
2022-08-02 15:55:38 +0530

    html change in upload page

--- src/app/global.js
+++ src/app/global.js
@@ -1,5 +1,5 @@
 // to run on server uncomment heroku server and vise versa
 
-//export const DJANGO_SERVER = "https://tdmdjangoapi.herokuapp.com/"      // heroku server
-export const DJANGO_SERVER = "http://localhost:8000/"                 // local server
+export const DJANGO_SERVER = "https://tdmdjangoapi.herokuapp.com/"      // heroku server
+//export const DJANGO_SERVER = "http://localhost:8000/"                 // local server
 export const developer = "Developer's Name: Mohamed Waqar Zulfeqar Dongre   email: waqardongre@gmail.com   Github: https://github.com/waqardongre"
